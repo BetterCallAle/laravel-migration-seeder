bee124dc69013009875d2183de48f4ab2d31fe76
BetterCallAle <dap22091998@gmail.com>
2022-12-19 16:52:20 +0100

    the train code is now on top of the ul

--- resources/views/partials/home-cards.blade.php
+++ resources/views/partials/home-cards.blade.php
@@ -1,12 +1,12 @@
 <div class="col">
     <div class="card bg-dark text-light p-3">
         <ul>
+            <li>Codice Treno: <strong>{{ $train->train_code }}</strong></li>
             <li>Compagnia: <strong>{{ $train->company }}</strong></li>
             <li>Stazione di partenza: <strong>{{ $train->departure_station }}</strong></li>
             <li>Stazione di arrivo: <strong> {{ $train->arrival_station }}</strong></li>
             <li>Partenza: <strong>{{ substr_replace($train->departure_time, '', 16, 3)}}</strong></li>
-            <li>Arrivo <strong>{{ substr_replace($train->arrival_time, '', 16, 3) }}</strong></li>
-            <li>Codice Treno: <strong>{{ $train->train_code }}</strong></li>
+            <li>Arrivo: <strong>{{ substr_replace($train->arrival_time, '', 16, 3) }}</strong></li>
             <li>Carrozze: <strong>{{ $train->total_carriages_num }}</strong></li>
             <li>
                 Treno in orario:
